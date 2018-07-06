1896128fe4c02a5782e78ff2769c814628d607f2
DESKTOP-9RG54CS\Daffa Mage <daffaabiyyum@gmail.com>
2018-07-06 10:46:57 +0700

    View Project Files

---
+++ PC/app/src/main/java/com/example/daffamage/pc/DetailedView.java
@@ -0,0 +1,13 @@
+package com.example.daffamage.pc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailedView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_view);
+    }
+}
