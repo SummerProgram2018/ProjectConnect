0934857c56299f4489e55e2d7b96f8280285140b
DESKTOP-9RG54CS\Daffa Mage <daffaabiyyum@gmail.com>
2018-07-08 16:35:30 +0700

    Added 1 activity connecting event (successful apply)

--- PC/app/src/main/java/com/example/daffamage/pc/DetailedView.java
+++ PC/app/src/main/java/com/example/daffamage/pc/DetailedView.java
@@ -3,8 +3,15 @@ package com.example.daffamage.pc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DetailedView extends AppCompatActivity {
+    public void goApply (View view)
+    {
+        Intent intent = new Intent(this, SuccessApply.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- PC/app/src/main/java/com/example/daffamage/pc/ViewActivity.java
+++ PC/app/src/main/java/com/example/daffamage/pc/ViewActivity.java
@@ -2,6 +2,7 @@ package com.example.daffamage.pc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ViewActivity extends AppCompatActivity {
 
