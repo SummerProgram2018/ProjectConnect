238e5c2519053d5787a69e54ea2b320ce179f88e
DESKTOP-9RG54CS\Daffa Mage <daffaabiyyum@gmail.com>
2018-07-12 10:51:40 +0700

    Make Project Success files

---
+++ Login/Login/app/src/main/java/com/example/kausthubram/login/MakeProjectSuccess.java
@@ -0,0 +1,13 @@
+package daffa.example.com.makeprojectsuccess;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakeProjectSuccess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_project_success);
+    }
+}
