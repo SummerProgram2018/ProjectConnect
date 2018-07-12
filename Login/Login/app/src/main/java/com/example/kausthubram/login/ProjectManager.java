8d6def63c4b8442c964e5590a5b3d5b99c4629cd
Chris Mathew <c.mathew@uqconnect.edu.au>
2018-07-12 22:46:20 +0800

    Added project manager page to app

---
+++ Login/Login/app/src/main/java/com/example/kausthubram/login/ProjectManager.java
@@ -0,0 +1,13 @@
+package com.example.kausthubram.login;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+public class ProjectManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_manager);
+    }
+}
